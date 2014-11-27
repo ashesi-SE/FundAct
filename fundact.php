f59248264a2265c630eb2e898edb4ff3addcd7a7
Nanette Taylor <nanette.m.taylor@gmail.com>
2014-11-27 04:19:09 +0000

    Changed the display of information from database to use angular. Embeded static youtube video

--- fundact.php
+++ fundact.php
@@ -5,16 +5,10 @@
 		function fundact(){
 			adb::adb();
 		}
-
 		function get_all_projects(){
 			$query = "SELECT * FROM projects";
 			return $this->query($query);
 		}
-
-        function insert_project($firstName, $lastname, $title, $description, $category, $target_amount, $video){
-            $query = "INSERT INTO projects (owner_fn,owner_ln,title,description,category,target_amount,video) VALUES ('$firstName', '$lastname', '$title', '$description', '$category', '$target_amount', '$video')";
-            return $this->query($query);
-        }
 	}
 ?>
 
--- sql/fundact.sql
+++ sql/fundact.sql
@@ -17,7 +17,7 @@ CREATE TABLE projects (
 INSERT INTO projects(owner_fn,owner_ln,title,description,category,target_amount,video) VALUES
 ("Carl","Agbenyega","Cool beans","some description","schools and supplies",500,"https://www.youtube.com/watch?feature=player_embedded&list=PLetBe2Jd1OJq8AJQbFMo_0oE2osSDna4N&v=utOVbVhU9zg"),
 ("Nanette","Taylor","NT creative solutions","some description","libraries",600,"https://www.youtube.com/watch?v=J-infmEWIbQ"),
-("Edem","Anaglo","Another creative educatoinal solution","ict tools an apps","some description",700,"https://www.youtube.com/watch?v=9EgCROOU6r8"),
+("Edem","Anaglo","Another creative educational solution","ict tools an apps","some description",700,"https://www.youtube.com/watch?v=9EgCROOU6r8"),
 ("Aba","Debrah","The brain behind the brain--wink wink","some description","other creative projects",800,"https://www.youtube.com/watch?v=7yqFhDoblPo");
 
 -- Select * from projects;
