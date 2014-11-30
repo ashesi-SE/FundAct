9aec068390665c4d3a20f37482f1bd2bc8433c58
Nanette Taylor <nanette.m.taylor@gmail.com>
2014-11-30 09:15:53 +0000

    Started an about us, how it works and projects page

--- aboutus.php
+++ aboutus.php
@@ -4,13 +4,14 @@
 
 	<div class="container">
 		<div class="row">
+			<h1>About Us</h1>
 			<p>Some paragraph talking about Fundact</p>
 		</div>
 	</div>
 	<div class="foundersBlock">
 		<div class="container">
 			<div class="center">
-				<h1>Founders Wall</h1>
+				<h1>Founders' Wall</h1>
 			</div>
 			<div class="row">
 				<div class="col-md-4">
--- index.php
+++ index.php
@@ -1,38 +1,17 @@
 <?php
-  include("header.html");
+	include("header.html");
 ?>
 
-<div ng-app="" ng-controller="projectsController">
-	<div class="container"> 
-		<div ng-repeat="project in projects">
+	<div class="homeBanner">
+		<div class="container">
 			<div class="row">
-				<div>{{ project.title }} - By {{ project.firstname + " " + project.lastname}}</div>
-			</div>
-			<div class="row">
-				<div class="col-md-4">
-					<div>
-						<b>Category: </b>{{project.category}}</br>
-						<b>Target Amount: </b>{{project.target_amount}}</br>
-						<b>Description: </b>{{project.description}}</br></br>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<iframe width="560" height="315" src="//www.youtube.com/embed/eY_mrU8MPfI?list=RDhbnPkK76Ask" frameborder="0" allowfullscreen></iframe>
-				</div>
+				<div class="title"><i class="fa fa-book fa-lg"></i> FundAct</div><br>
+				<div class="slogan">A platform where donors and educational projects can interact</div><br>
+				<a class="btn btn-primary btn-lg" href="howitworks.php" role="button">Learn more</a>
 			</div>
 		</div>
 	</div>
-</div>
-
-<script>
-function projectsController($scope,$http) {
-    //var site = "fundact";
-    //var page = "fundact/fundact_action.php?function=get_all_projects";
-    $http.get("fundact_action.php?function=get-all_projects")
-    .success(function(response) {$scope.projects = response;});
-}
-</script>
 
 <?php
-include("footer.html")
+	include("footer.html");
 ?>
