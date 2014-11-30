2dcb6b8ac38b5a5a9fd51a524c2ed43f2a2200e5
Nanette Taylor <nanette.m.taylor@gmail.com>
2014-11-30 09:21:59 +0000

    Started an about us, how it works and projects page

---
+++ projects.php
@@ -0,0 +1,36 @@
+<?php
+  include("header.html");
+?>
+
+<div ng-app="" ng-controller="projectsController">
+	<div class="container"> 
+		<div ng-repeat="project in projects">
+			<div class="row">
+				<div>{{ project.title }} - By {{ project.firstname + " " + project.lastname}}</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div>
+						<b>Category: </b>{{project.category}}</br>
+						<b>Target Amount: </b>{{project.target_amount}}</br>
+						<b>Description: </b>{{project.description}}</br></br>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<iframe width="560" height="315" src="//www.youtube.com/embed/eY_mrU8MPfI?list=RDhbnPkK76Ask" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+function projectsController($scope,$http) {
+    $http.get("fundact_action.php?function=get-all_projects")
+    .success(function(response) {$scope.projects = response;});
+}
+</script>
+
+<?php
+include("footer.html")
+?>
