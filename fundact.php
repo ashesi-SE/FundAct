3d102e76c6b50bb5bcee2a1949627cfd71c91d4d
Aba Debrah <abadee39@gmail.com>
2014-12-01 22:16:02 +0000

    second-iteration

--- fundact.php
+++ fundact.php
@@ -9,6 +9,13 @@
 			$query = "SELECT * FROM projects";
 			return $this->query($query);
 		}
+
+		function add_projects($owner_firstname, $owner_lastname, $title, $description, $category, $target_amount, $video){
+			$query = "INSERT INTO projects SET owner_fn ='$owner_firstname', owner_ln='$owner_lastname', title='$title',
+			 description='$description', category='$category', target_amount='$target_amount', video='$video'";
+		return $this->query($query);
+		}
+
 	}
 ?>
 
