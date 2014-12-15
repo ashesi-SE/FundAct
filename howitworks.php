66e57ff73546df2f4ba0547bff0e605050a4319f
Aba Debrah <abadee39@gmail.com>
2014-12-15 12:20:14 +0000

    editing how-it-works page & semantic UI

--- howitworks.php
+++ howitworks.php
@@ -1,34 +1,50 @@
 <?php
 	include("header.html");
 ?>
+<div class="container bootcards-container">
+<div align="center">
+	<h1> Steps</h1>
+<div class="ui cards " >
+  <div class="card panel panel-default">
+    <div class="image">
+      <img src="images/step-1.jpg">
+    </div>
+    <div class="content">
+      <div class="description">
+        <b class="text">Put up your project and set your target amount and date</b>
+      </div>
+    </div>
+  </div>
 
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4">
-				Step 1
-			</div>
-			<div class="col-md-8">
-				Put up your project and set your target amount and date
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-4">
-				Step 2
-			</div>
-			<div class="col-md-8">
-				Sit back and relax while donors pledge money for your project. You can promote your project through social media to help.
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-4">
-				Step 3
-			</div>
-			<div class="col-md-8">
-				When you've got enough pledges to start your project. The money is taken from the accounts of donors and transfered to your account
-			</div>
-		</div>
-	</div>	
+  <div class="card panel panel-default">
+    <div class="image">
+      <img src="images/step-2.jpg">
+    </div>
+    <div class="content">
+      <div class="description">
+        <b class="text">Sit back and relax while donors pledge money for your project.</b> 
+        <br></br>
+        	<b class="text">You can promote your project through social media to help.</b>
+      </div>
+    </div>
+  </div>
 
+  <div class="card panel panel-default">
+    <div class="image">
+      <img src="images/step-3.jpg">
+    </div>
+    <div class="content">
+      <div class="description">
+       <b class="text"> When you've got enough pledges to start your project. </b>
+       <br></br>
+       	<b class="text">The money is taken from the accounts of donors and transfered to your account</b>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+</div>
 <?php
 	include("footer.html");
 ?>
